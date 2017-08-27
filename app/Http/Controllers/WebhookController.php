aa86464c70d3eb718e7fd750a7926d708c9c7151
theardent <oleksii.yaryi@gmail.com>
2017-08-27 16:14:14 +0300

    add package for Gitter Api

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Gitter\Client;
 use Illuminate\Http\Request;
+use Log;
 use Telegram;
 
 class WebhookController extends Controller
@@ -10,18 +13,44 @@ class WebhookController extends Controller
 	public function telegram()
 	{
 		$update = Telegram::getWebhookUpdates();
-		$message = $update->getMessage();
 
-		$chat_name = $message->getChat()->getTitle();
-		$user_name = $message->getFrom()->getUsername();
+		try {
+			$message = $update->getMessage();
+			if (is_null($message)) {
+				return;
+			}
+			$chat_name = $message->getChat()->getTitle();
+			$chat_id = $message->getChat()->getId();
+			$user_name = $message->getFrom()->getUsername();
 
-		\Log::info(
-			'message',
-			[
-				'chat_name' => $chat_name,
-				'username'  => $user_name,
-				'text'      => $message->getText()
-			]
-		);
+			Log::info(
+				'message',
+				[
+					'chat_name' => $chat_name,
+					'chat_id'   => $chat_id,
+					'username'  => $user_name,
+					'text'      => $message->getText()
+				]
+			);
+			if (!strcmp($chat_name,env('TELEGRAM_BOT_GROUP'))) {
+				$client = new Client(env('GITTER_TOKEN'));
+				$client->connect();
+				$room = $client->rooms->findByName(env('GITTER_ROOM_NAME'));
+				$text = "@{$user_name} {$message->getText()}";
+				$client->messages->create($room['id'], $text);
+			}
+		}
+		catch (Exception $exception) {
+			Log::error(
+				'Exception',
+				[
+					'message' => $exception->getMessage(),
+					'code'    => $exception->getCode(),
+					'trace'   => $exception->getTraceAsString()
+				]
+			);
+		}
+
+
 	}
 }
